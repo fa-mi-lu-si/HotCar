aee419aa0533684ab84276df4a3cbdbcea8bdaea
Samuel Familusi <sfamilusi51@outlook.com>
2024-04-19 15:12:18 +0200

    Made the SMS work

--- A9GTest.py
+++ A9GTest.py
@@ -19,25 +19,40 @@ def make_call(number):
     print(f"Call placed to {number}")
 
 def send_sms(number, text):
-    response = send_at_command("AT+CMGF=1;")
+    response = send_at_command("AT+CMGF=1")
 
     if not response or ("OK" not in response):
-        print("Failed to send text")
+        print("Failed to set SMS format")
         return
     
-    response = send_at_command('AT+CMGS="{}"'.format(number))
+    response = send_at_command(f'AT+CMGS="{number}"')
     if not response or (">" not in response):
         print("Failed to set recipient number")
         return
     
     response = send_at_command(text)
-    print(response)
     if not response:
-        print("Failed to send message")
+        print("Failed to send message.")
+        print(f"Response: {response}")
         return
+    
+    print(f"Response (success) : {response}")
     
     uart.write(chr(26))
     sleep(1)
+
+def receive_sms():
+    response = send_at_command("AT+CMGF=1")
+    if not response or ("OK" not in response):
+        print("Failed to set SMS format")
+        return
+
+    response = send_at_command('AT+CMGL="ALL"')
+    print(response)
+
+def delete_sms():
+    response = send_at_command('AT+CMGDA="DEL ALL"')
+    print(response)
 
 def test_connection():
     response = send_at_command('AT')
@@ -51,21 +66,7 @@ def test_connection():
 def gps_read():
     print(send_at_command("AT+GPS=1"))
 
-    print(send_at_command("AT+GPSRD=1"))
-    sleep(2)
-    return uart.read().decode()
-
-def is_gps_connected(gps_response):
-    # Split the response into individual sentences
-    sentences = gps_response.split('\n')
-    for sentence in sentences:
-        # Check if the sentence starts with "$GNRMC"
-        if sentence.startswith("$GNRMC"):
-            # Split the sentence by commas
-            sentence_parts = sentence.split(',')
-            # Check if the status field is 'A' (Active)
-            if len(sentence_parts) >= 12 and sentence_parts[2] == 'A':
-                return True
-    return False
-
-# make_call("0714453474")
+    # print(send_at_command("AT+GPSRD=1"))
+    gps_data = send_at_command("AT+LOCATION=1")
+    
+    return gps_data
